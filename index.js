a86fa7b0f2615b0e0ee43b8305aade55d9f51456
parth <parthbatra1908@gmail.com>
2025-04-21 12:14:20 +0530

    added the create and read functionality in the notes app

---
+++ index.js
@@ -0,0 +1,44 @@
+const express=require('express')
+const path=require("path")
+const fs=require("fs");
+const app=express();
+
+app.set('view engine','ejs');
+// setting the view engine as ejs 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+
+// line to work and use static files/ serving static files with express 
+app.use(express.static(path.join(__dirname,"public")))
+// static files are images Audio,vedio js vanilla js, css files etc 
+
+app.get("/",(req,res)=>{
+    // res.send("Hello, from Server!");
+    // view folder ke items show karne ke liye ham render karte hein naa ki send 
+    // res.render('index');
+
+    // iss route pe ham files folder ke andar kitni files hein vo dekhnge using fs module.
+    fs.readdir(`./files`,(err,files)=>{
+        res.render("index",{files:files})
+        // index view ejs mein files name se files wala data bheja hein 
+        // ham iske through view page pe kuch bhi bhej sakte hein jo hmara man karein 
+    })
+
+
+})
+app.post("/create",(req,res)=>{
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.description,function(err){
+        res.redirect("/");
+    })
+})
+app.get("/file/:filename",(req,res)=>{
+    fs.readFile(`./files/${req.params.filename}`,"utf-8",(err,filedata)=>{
+        res.render('show',{filename:req.params.filename,filedata:filedata});
+    })
+}
+    )
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000")
+})
---
+++ readme.txt
@@ -0,0 +1,1 @@
+notes making web app.
