7d57366c8a0578399facbb2d003ec09f925af582
parth <parthbatra1908@gmail.com>
2025-04-26 12:01:25 +0530

    added the edit option for title of the notes

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ app.get("/file/:filename",(req,res)=>{
     })
 }
     )
+    app.get("/edit/:filename",(req,res)=>{
+        res.render('edit',{filename:req.params.filename});
+    })
+    app.post("/edit",(req,res)=>{
+       fs.rename(`./files/${req.body.previous}`,`./files/${req.body.new}`,(err)=>{
+        res.redirect("/")
+       })
+    })
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
